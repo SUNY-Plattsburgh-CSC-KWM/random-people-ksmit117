c5618fa84c19fd252f7787fa9d0ce1f914a22715
ksmit117 <158485702+ksmit117@users.noreply.github.com>
2025-10-23 14:14:38 -0400

    yes

--- PeopleDashboardTemplate.js
+++ PeopleDashboardTemplate.js
@@ -15,10 +15,35 @@ async function getPeople() {
 async function buildTable() {
 	try {
 		const data = await getPeople();
+		const people = data.results;
+
+		people.forEach(person => {
+			const name = `${person.first = person.name.first, person.last = person.name.last}`;
+			const address = `${person.address = person.location.street.number}`;
+			const city = `${person.city = person.location.city}`;
+			const state = `${person.state = person.location.state}`;
+			const zip = `${person.zip = person.location.postcode}`;
+			const longitude = `${person.longitude = person.location.coordinates.longitude}`;
+			const latitude = `${person.latitude = person.location.coordinates.latitude}`;
+			console.log(`Name: ${name}, Address: ${address}, City: ${city}, State: ${state}, Zip: ${zip}, Longitude: ${longitude}, Latitude: ${latitude}`);
+
+		$("#people tbody").append(`
+			<tr>
+				<td>${name}</td>
+				<td>${address}</td>
+				<td>${city}</td>
+				<td>${state}</td>
+				<td>${zip}</td>
+				<td>${longitude}</td>
+				<td>${latitude}</td>
+			</tr>
+			`);
+			});
 	} catch (e) {
 		console.log("Error " + e);
 	}
 }
 
-buildTable();
-
+$(document).ready(function() {
+	buildTable();
+});
